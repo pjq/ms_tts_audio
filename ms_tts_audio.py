018d1847b84b43f50a716e620179134098c43100
Jianqing Peng <Jianqing.Peng@sap.com>
2023-11-24 19:11:06 +0800

    fix the script

--- ms_tts_audio.py
+++ ms_tts_audio.py
@@ -8,28 +8,6 @@ if __name__ == "__main__":
     parser.add_argument("--output", dest="output", type=str, default="output", help="output audio file name")
     parser.add_argument("--subscription_key", dest="subscription_key", type=str, required=True, help="subscription key")
     parser.add_argument("--service_region", dest="service_region", type=str, required=True, help="service region")
-    options = parser.parse_args()
-
-    if options.text:
-        text = options.text
-    elif options.inputfile:
-        with open(options.inputfile, "r") as file:
-            text = file.read()
-    else:
-        raise ValueError("Either a text or a valid inputfile must be provided.")
-
-
-
-import argparse
-from ms_tts_service import service as text2mp3
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--text", dest="text", type=str, default=None, help="text to synthesize")
-    parser.add_argument("--input", dest="inputfile", type=str, default=None, help="input file to read text from")
-    parser.add_argument("--output", dest="output", type=str, default="output", help="output audio file name")
-    parser.add_argument("--subscription_key", dest="subscription_key", type=str, required=True, help="subscription key")
-    parser.add_argument("--service_region", dest="service_region", type=str, required=True, help="service region")
     parser.add_argument("--voice_name", dest="voice_name", type=str, default="zh-CN-YunyangNeural", help="voice name for tts engine")
     options = parser.parse_args()
 
